20f3973ceed432739b740e4451cc2dff7cef0d53
Rukkiah Sajeena M <144934352+sajeena3012@users.noreply.github.com>
2024-07-05 21:55:15 +0530

    Remove duplicates.java

---
+++ Arrays/Remove duplicates.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int removeDuplicates(int[] nums) {
+        if (nums.length == 0) {
+            return 0;
+        }
+
+        int j = 1; // pointer for the place to insert the next unique element
+
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                j++;
+            }
+        }
+
+        return j;
+    }
+}
