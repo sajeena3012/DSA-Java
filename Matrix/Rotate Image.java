e985cf93cc017a92c6e6c97582043c015e07c62b
Rukkiah Sajeena M <144934352+sajeena3012@users.noreply.github.com>
2024-08-20 00:21:59 +0530

    Add files via upload

---
+++ Matrix/Rotate Image.java
@@ -0,0 +1,25 @@
+class Solution{
+    public void rotate(int[][] matrix) {
+    int n = matrix.length;
+
+    for (int i = 0; i < (n + 1) / 2; i++) {
+        for (int j = 0; j < n / 2; j++) {
+            // Start 4-way swaps
+            // temp = bottom left
+            int temp = matrix[n - 1 - i][j];
+
+            // bottom left = bottom right
+            matrix[n - 1 - i][j] = matrix[n - 1 - j][n - 1 - i];
+
+            // bottom right = top right
+            matrix[n - 1 - j][n - 1 - i] = matrix[i][n - 1 - j];
+
+            // top right = top left
+            matrix[i][n - 1 - j] = matrix[j][i];
+
+            // top left = temp
+            matrix[j][i] = temp;
+        }
+    }
+  }
+}
