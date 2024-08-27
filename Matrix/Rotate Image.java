689429922d5e881fc2145c8c02d76b99ff634028
Rukkiah Sajeena M <144934352+sajeena3012@users.noreply.github.com>
2024-08-27 21:37:35 +0530

    Update Rotate Image.java

--- Matrix/Rotate Image.java
+++ Matrix/Rotate Image.java
@@ -11,15 +11,4 @@ class Solution{
             // bottom left = bottom right
             matrix[n - 1 - i][j] = matrix[n - 1 - j][n - 1 - i];
 
-            // bottom right = top right
-            matrix[n - 1 - j][n - 1 - i] = matrix[i][n - 1 - j];
-
-            // top right = top left
-            matrix[i][n - 1 - j] = matrix[j][i];
-
-            // top left = temp
-            matrix[j][i] = temp;
-        }
-    }
-  }
-}
+         
